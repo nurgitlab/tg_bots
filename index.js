389c4c6519d45d516c9defd51ad316651e6eb7e0
nurgitlab <nurbek.math@gmail.com>
2024-05-11 16:16:55 +0300

    refactor: main file

--- index.js
+++ index.js
@@ -6,97 +6,89 @@ const sleep = require(path.join(__dirname, "src/utils/sleep.js"))
 const {isWin} = require("./src/utils/isWin");
 
 bot.setMyCommands([
-    {command: "/start", description: "Начальное приветствие"},
-    {command: "/info", description: "Информация"},
-    {command: "/coin", description: "Бросить монетку"},
+  {command: "/start", description: "Начальное приветствие"},
+  {command: "/info", description: "Информация"},
 ])
 
 bot.on("message", async msg => {
-    const text = msg.text, chatId = msg.chat.id;
+  const text = msg.text, chatId = msg.chat.id;
 
-    if (text === "/start") {
-        await bot.sendMessage(chatId, `Выберите Орёл или Решка`, {
-            reply_markup: {
-                keyboard: [
-                    ['⭐️ Орёл', '⭐️ Решка'],
-                    ['❌ Закрыть меню']
-                ],
-                resize_keyboard: true
-            }
-        })
+  const win = async (text, url) => {
+    await bot.sendSticker(chatId, path.join(__dirname, url))
+    await bot.sendMessage(chatId, text)
+    await bot.sendSticker(chatId, path.join(__dirname, 'assets/stickers/win.tgs'))
+  }
 
-        return bot.sendMessage(chatId, "Добро пожаловать в монетку")
+  const lose = async (text, url) => {
+    await bot.sendSticker(chatId, path.join(__dirname, url))
+    await bot.sendMessage(chatId, text)
+    await bot.sendSticker(chatId, path.join(__dirname, 'assets/stickers/lose.tgs'))
+  }
+
+  if (text === "/start") {
+    await bot.sendMessage(chatId, `Выберите Орёл или Решка`, {
+      reply_markup: {
+        keyboard: [
+          ['⭐️ Орёл', '⭐️ Решка'],
+          ['❌ Закрыть меню']
+        ],
+        resize_keyboard: true
+      }
+    })
+
+    return bot.sendMessage(chatId, "Добро пожаловать в монетку")
+  }
+  if (text === "/info") {
+    return bot.sendMessage(chatId, `Ваши инициалы: ${msg.from.first_name}`)
+  }
+
+  if (text == '⭐️ Орёл') {
+    await bot.sendSticker(chatId, path.join(__dirname, 'assets/stickers/st1.tgs'))
+    await bot.sendMessage(chatId, "Вы загадали Орла, идёт бросок монетки, ждите")
+
+    if (isWin(99)) {
+      await win("Вы победили, ВЫПАЛ ОРЁЛ!!!", 'assets/stickers/9.png')
+    } else {
+      await lose("Решка, Попробуйте снова :(", 'assets/stickers/10.png')
     }
-    if (text === "/info") {
-        return bot.sendMessage(chatId, `Ваши инициалы: ${msg.from.first_name}`)
-    }
-    if (text === "/coin" ) {
+    return;
+  }
 
-    }
+  if (text == '⭐️ Решка') {
+    await bot.sendSticker(chatId, path.join(__dirname, 'assets/stickers/st1.tgs'))
+    await bot.sendMessage(chatId, "Вы загадали Решку, идёт бросок монетки, ждите")
 
-    if (text == '⭐️ Орёл') {
-        await bot.sendSticker(chatId, path.join(__dirname, 'assets/stickers/st1.tgs'))
-        setTimeout(async () => {
-            await bot.sendMessage(chatId, "Вы загадали Орла, идёт бросок монетки, ждите")
-        }, 100)
-
-        setTimeout(async () => {
-            if (isWin(99)) {
-                await bot.sendSticker(chatId, path.join(__dirname, 'assets/stickers/9.png'))
-                await bot.sendMessage(chatId, "Вы победили, ВЫПАЛ ОРЁЛ!!!")
-                await bot.sendSticker(chatId, path.join(__dirname, 'assets/stickers/win.tgs'))
-            } else {
-                await bot.sendSticker(chatId, path.join(__dirname, 'assets/stickers/10.png'))
-                await bot.sendMessage(chatId, "Решка, Попробуйте снова :(")
-                await bot.sendSticker(chatId, path.join(__dirname, 'assets/stickers/lose.tgs'))
-            }
-        }, 500)
-        return;
-    }
-
-    if (text == '⭐️ Решка') {
-        await bot.sendSticker(chatId, path.join(__dirname, 'assets/stickers/st1.tgs'))
-        setTimeout(async () => {
-            await bot.sendMessage(chatId, "Вы загадали Решку, идёт бросок монетки, ждите")
-        }, 100)
-
-        setTimeout(async () => {
-            if (isWin(99)) {
-                await bot.sendSticker(chatId, path.join(__dirname, 'assets/stickers/10.png'))
-                await bot.sendMessage(chatId, "Вы победили, ВЫПАЛА РЕШКА!!!")
-                await bot.sendSticker(chatId, path.join(__dirname, 'assets/stickers/win.tgs'))
-
-            } else {
-                await bot.sendSticker(chatId, path.join(__dirname, 'assets/stickers/9.png'))
-                await bot.sendMessage(chatId, "Орёл, Попробуйте снова :(")
-                await bot.sendSticker(chatId, path.join(__dirname, 'assets/stickers/lose.tgs'))
-            }
-        }, 500)
-        return;
+    if (isWin(99)) {
+      await win("Вы победили, ВЫПАЛА РЕШКА!!!", 'assets/stickers/10.png')
+    } else {
+      await lose("Орёл, Попробуйте снова :(", 'assets/stickers/9.png')
     }
 
-    if(msg.text == '❌ Закрыть меню') {
-        return await bot.sendMessage(chatId, 'Меню закрыто', {
-            reply_markup: {
-                keyboard: [
-                    ['Открыть меню']
-                ],
-                resize_keyboard: true
-            }
-        })
-    }
+    return;
+  }
 
-    if (text === "Открыть меню") {
-        return await bot.sendMessage(chatId, `Выберите Орёл или Решка`, {
-            reply_markup: {
-                keyboard: [
-                    ['⭐️ Орёл', '⭐️ Решка'],
-                    ['❌ Закрыть меню']
-                ],
-                resize_keyboard: true
-            }
-        })
-    }
+  if (msg.text == '❌ Закрыть меню') {
+    return await bot.sendMessage(chatId, 'Меню закрыто', {
+      reply_markup: {
+        keyboard: [
+          ['Открыть меню']
+        ],
+        resize_keyboard: true
+      }
+    })
+  }
 
-    bot.sendMessage(chatId, "Я тебя не понимаю")
+  if (text === "Открыть меню") {
+    return await bot.sendMessage(chatId, `Выберите Орёл или Решка`, {
+      reply_markup: {
+        keyboard: [
+          ['⭐️ Орёл', '⭐️ Решка'],
+          ['❌ Закрыть меню']
+        ],
+        resize_keyboard: true
+      }
+    })
+  }
+
+  bot.sendMessage(chatId, "Я тебя не понимаю")
 })
